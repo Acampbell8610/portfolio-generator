1a8afe08308a0f2d96ab1c3345ad2748349f3ded
acamp <acampbell8610@gmail.com>
2022-02-17 21:28:17 -0600

    finished site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
- const fs = require('fs');
- const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 
 const promptUser = () => {
@@ -133,17 +133,47 @@ Add a New Project
     }
   });
 };
-
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    
-    
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+//moved to rhe generate sitejs
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+    
+    
+//     const pageHTML = generatePage(portfolioData);
+
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Page created! Check out index.html in this directory to see it!');
+    
+//       fs.copyFile('./src/style.css', './dist/style.css', err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('Style sheet copied successfully!');
+//       });
+//     });
+  
+//   });
